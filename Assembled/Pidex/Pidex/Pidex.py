75c99da1af4530500d6792c1e9eb40b1a07def4c
TobiHatti <32573581+TobiHatti@users.noreply.github.com>
2019-03-14 09:27:46 +0100

    Added Assebled build

---
+++ Assembled/Pidex/Pidex/Pidex.py
@@ -0,0 +1,148 @@
+# Importing Modules
+import pygame
+import time
+import random
+import sys
+import math
+
+# Color Definitions
+white = (255,255,255)
+grayA = (200,200,200)
+grayB = (150,150,150)
+grayC = (100,100,100)
+black = (0,0,0)
+
+# PyGame Initialisation
+pygame.init()
+clock = pygame.time.Clock()
+#pygame.mouse.set_visible(False)
+
+# Window and Surface Initialisation
+displayWidth = 800
+displayHeight = 480
+
+mainSurface = pygame.display.set_mode((displayWidth,displayHeight))
+
+# Variables
+menuOptionSelected = False
+selectedMenuOption = 0
+
+# Functions
+
+# Defining Segment-Polygons
+# Top segments
+pSegment1 = ((0,0),(displayWidth/2,0),(displayWidth/2,displayHeight/2),(0,displayHeight/6))
+pSegment2 = ((displayWidth,displayHeight/6),(displayWidth,0),(displayWidth/2,0),(displayWidth/2,displayHeight/2))
+# Middle segments
+pSegment3 = ((0,displayHeight/6),(displayWidth/2,displayHeight/2),(0,displayHeight-(displayHeight/6)))
+pSegment4 = ((displayWidth,displayHeight/6),(displayWidth/2,displayHeight/2),(displayWidth,displayHeight-(displayHeight/6)))
+# Botton Section
+pSegment5 = ((0,displayHeight),(0,displayHeight-(displayHeight/6)),(displayWidth/2,displayHeight/2),(displayWidth/3,displayHeight))
+pSegment6 = ((displayWidth/3,displayHeight),(displayWidth/2,displayHeight/2),(displayWidth-(displayWidth/3),displayHeight))
+pSegment7 = ((displayWidth,displayHeight),(displayWidth,displayHeight-(displayHeight/6)),(displayWidth/2,displayHeight/2),(displayWidth-(displayWidth/3),displayHeight))
+
+
+while not menuOptionSelected:
+
+    # Event Processing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Keypress Processing
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q] != 0: 
+        pygame.quit()
+        sys.exit()
+
+    # Checking Mouse Positions
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    # Segment 1
+    if 0 < mouse[0] < displayWidth/2 and 0 < mouse[1] < displayHeight/3:
+        pygame.draw.polygon(mainSurface,black,pSegment1)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 1
+    else: pygame.draw.polygon(mainSurface,grayA,pSegment1)
+
+    # Segment 2
+    if displayWidth/2 < mouse[0] < displayWidth and 0 < mouse[1] < displayHeight/3:
+        pygame.draw.polygon(mainSurface,black,pSegment2)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 2
+    else: pygame.draw.polygon(mainSurface,grayB,pSegment2)
+
+    # Segment 3
+    if 0 < mouse[0] < displayWidth/3 and displayHeight/3 < mouse[1] < displayHeight-(displayHeight/3):
+        pygame.draw.polygon(mainSurface,black,pSegment3)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 3
+    else: pygame.draw.polygon(mainSurface,grayB,pSegment3)
+
+    # Segment 4
+    if displayWidth-(displayWidth/3) < mouse[0] < displayWidth and displayHeight/3 < mouse[1] < displayHeight-(displayHeight/3):
+        pygame.draw.polygon(mainSurface,black,pSegment4)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 4
+    else: pygame.draw.polygon(mainSurface,grayA,pSegment4)
+
+    # Segment 5
+    if 0 < mouse[0] < displayWidth/3 and displayHeight-(displayHeight/3) < mouse[1] < displayHeight:
+        pygame.draw.polygon(mainSurface,black,pSegment5)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 5
+    else: pygame.draw.polygon(mainSurface,grayA,pSegment5)
+
+    # Segment 6
+    if displayWidth/3 < mouse[0] < displayWidth-(displayWidth/3) and displayHeight-(displayHeight/3) < mouse[1] < displayHeight:
+        pygame.draw.polygon(mainSurface,black,pSegment6)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 6
+    else: pygame.draw.polygon(mainSurface,grayC,pSegment6)
+
+    # Segment 7
+    if displayWidth-(displayWidth/3) < mouse[0] < displayWidth and displayHeight-(displayHeight/3) < mouse[1] < displayHeight:
+        pygame.draw.polygon(mainSurface,black,pSegment7)
+        if click[0] == 1: 
+            menuOptionSelected = True
+            selectedMenuOption = 7
+    else: pygame.draw.polygon(mainSurface,grayB,pSegment7)
+
+
+    # Center
+    pygame.draw.circle(mainSurface,(0,0,0),(int(displayWidth/2),int(displayHeight/2)),100)
+    pygame.draw.circle(mainSurface,(255,255,255),(int(displayWidth/2),int(displayHeight/2)),85)
+
+    pygame.display.update()
+
+    
+
+
+for x in range(0, 500):
+    if x%2 == 0:
+        pygame.draw.circle(mainSurface,(255,255,255),(int(displayWidth/2),int(displayHeight/2)),100+x)
+        pygame.display.update()
+
+
+if selectedMenuOption == 1:
+    print("Option 1")
+elif selectedMenuOption == 2:
+    print("Option 2")
+elif selectedMenuOption == 3:
+    print("Option 3")
+elif selectedMenuOption == 4:
+    print("Option 4")
+elif selectedMenuOption == 5:
+    print("Option 5")
+elif selectedMenuOption == 6:
+    print("Option 6")
+elif selectedMenuOption == 7:
+    print("Option 7")
---
+++ Units/DexMenuScreen/DexMenuScreen/basics.py
@@ -0,0 +1,76 @@
+class Basic:
+
+    def WriteText(surface,pos,text,fontSize,fontFamily,fontColor,centered = False):
+        
+        import pygame
+
+        outputText = pygame.font.Font(fontFamily,fontSize)
+        textSurf = outputText.render(text,True,fontColor)
+        textRect = outputText.render(text,True,fontColor).get_rect()
+        if centered:
+            textRect.center = (pos[0],pos[1])
+        else:
+            textRect = (pos[0],pos[1])
+        surface.blit(textSurf,textRect)
+
+    def BlitText(surface, text, pos, font, color=(0,0,0)):
+        words = [word.split(' ') for word in text.splitlines()]
+        space = font.size(' ')[0] 
+        max_width, max_height = surface.get_size()
+        x, y = pos
+        for line in words:
+            for word in line:
+                word_surface = font.render(word, 0, color)
+                word_width, word_height = word_surface.get_size()
+                if x + word_width >= max_width:
+                    x = pos[0] 
+                    y += word_height 
+                surface.blit(word_surface, (x, y))
+                x += word_width + space
+            x = pos[0] 
+            y += word_height 
+
+    def Button(surface,pos,radius,text,fontSize,idleColor,textColor,action = None,parameter = None,surfaceOffset = (0,0), buttonEnabled = True):
+        
+        import pygame
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        hoverColor = (idleColor[0]/2,idleColor[1]/2,idleColor[2]/2)
+
+        if pos[0] + radius + surfaceOffset[0] > mouse[0] > pos[0] - radius + surfaceOffset[0] and pos[1] + radius + surfaceOffset[1] > mouse[1] > pos[1] - radius + surfaceOffset[1] and click[0]==1:
+
+            pygame.gfxdraw.aacircle(surface,pos[0]-2,pos[1]-2,radius-4,hoverColor)
+            pygame.gfxdraw.filled_circle(surface,pos[0]-2,pos[1]-2,radius-4,hoverColor)
+            if action != None and buttonEnabled:
+                if parameter == None: action()
+                else: action(parameter)
+        else:
+            pygame.gfxdraw.aacircle(surface,pos[0]-2,pos[1]-2,radius-4,idleColor)
+            pygame.gfxdraw.filled_circle(surface,pos[0]-2,pos[1]-2,radius-4,idleColor)
+
+        Basic.WriteText(surface,pos,text,fontSize,"PokemonSolid.ttf",textColor,True)
+
+
+    def ProgressBar(surface,pos,width,height,color,text,minValue,maxValue,value):
+
+        import pygame
+
+        white = (255,255,255)
+        displayWidth = int(width/((maxValue-minValue+1)/(value-minValue+1)))-1
+
+        pygame.gfxdraw.aacircle(surface,pos[0],pos[1],int(height/2),white)
+        pygame.gfxdraw.filled_circle(surface,pos[0],pos[1],int(height/2),white)
+        pygame.gfxdraw.aacircle(surface,pos[0]+width,pos[1],int(height/2),white)
+        pygame.gfxdraw.filled_circle(surface,pos[0]+width,pos[1],int(height/2),white)
+        pygame.gfxdraw.box(surface,(pos[0],pos[1]-int(height/2),width,height+1),white)
+
+        pygame.gfxdraw.aacircle(surface,pos[0],pos[1],int(height/2)-3,color)
+        pygame.gfxdraw.filled_circle(surface,pos[0],pos[1],int(height/2)-3,color)
+        pygame.gfxdraw.aacircle(surface,pos[0]+displayWidth,pos[1],int(height/2)-3,color)
+        pygame.gfxdraw.filled_circle(surface,pos[0]+displayWidth,pos[1],int(height/2)-3,color)
+        pygame.gfxdraw.box(surface,(pos[0],pos[1]-int(height/2)+3,displayWidth,height+1-6),color)
+
+        Basic.WriteText(surface,(pos[0],pos[1]-30),text,20,"calibrilight.ttf",white)
+        Basic.WriteText(surface,(pos[0]+width+30,pos[1]),str(value),20,"calibrilight.ttf",white,True)
